24b14a9cf5bc58946ebf998491929d10f369b619
342300393 <342300393@qq.com>
2016-07-09 21:05:04 +0800

    7.9晚

--- www/js/app.js
+++ www/js/app.js
@@ -77,17 +77,18 @@ angular.module('starter', ['ionic','starter.homeCtrl',
       }
     });
   $urlRouterProvider.otherwise('/tab');
-   $ionicConfigProvider.platform.ios.tabs.style('standard');   
-    $ionicConfigProvider.platform.ios.tabs.position('bottom');
-    $ionicConfigProvider.platform.android.tabs.style('standard');
-    $ionicConfigProvider.platform.android.tabs.position('standard');
+   $ionicConfigProvider.platform.ios.tabs.style('standard');
+   $ionicConfigProvider.platform.ios.tabs.position('bottom');
+   $ionicConfigProvider.platform.android.tabs.style('standard');
+   $ionicConfigProvider.platform.android.tabs.position('standard');
 
-    $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
-    $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+   $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+   $ionicConfigProvider.platform.android.navBar.alignTitle('left');
 
-    $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
-    $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+   $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+   $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
 
-    $ionicConfigProvider.platform.ios.views.transition('ios');
-    $ionicConfigProvider.platform.android.views.transition('android');
+   $ionicConfigProvider.platform.ios.views.transition('ios');
+   $ionicConfigProvider.platform.android.views.transition('android');
+
 })

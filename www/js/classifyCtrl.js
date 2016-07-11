0b506af01e6be79627036331cf314ea93ae4ca85
342300393 <342300393@qq.com>
2016-07-11 09:30:04 +0800

    classify

--- www/js/classifyCtrl.js
+++ www/js/classifyCtrl.js
@@ -1,4 +1,15 @@
-angular.module('starter.classifyCtrl', [])
-.controller('classifyController',['$scope',function( $scope ){
-
+angular.module('starter.classifyCtrl', ['ionic'])
+.controller('classifyController',['$scope','$http',function( $scope,$http){
+  $scope.did=function(re){
+    console.log(re);
+  }
+$http.get('./data/classify.json')
+.then(
+  function(res){
+    $scope.lists=res.data.data.title;
+  },function(res){
+    console.log(res);
+  }
+)
 }])
+.config(function(){})

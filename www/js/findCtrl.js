7eac550e686f3e3fc10a507a7820ec3ac9e18ba0
342300393 <342300393@qq.com>
2016-07-12 20:50:26 +0800

    上拉加载下拉刷新

--- www/js/findCtrl.js
+++ www/js/findCtrl.js
@@ -22,13 +22,50 @@ angular.module('starter.findCtrl', ['ionic'])
       controller : 'findPageCtroller'
       })
   }])
-  .controller('findPageCtroller',['$scope','$http',function( $scope ,$http){
+  .controller('findPageCtroller',['$scope','$http','$ionicLoading','$timeout',function( $scope ,$http, $ionicLoading , $timeout){
       $http.get('./data/findPage.json')
         .then(function( res ){
-          console.log(res.data.detaillist);
            $scope.listpage = res.data.listpage;
            $scope.detaillist = res.data.detaillist;
         },function( res ){
             $scope.listpage = res.data.error;
-        })
+        });
+        // 下拉刷新
+      $scope.doRefresh = function() {
+        $http.get('./data/findPage.json')
+         .success(function( res ) {
+           $scope.items = [];
+         })
+         .finally(function() {
+           // Stop the ion-refresher from spinning
+           $scope.$broadcast('scroll.refreshComplete');
+         });
+      };
+      // 上拉加载
+      $scope.items = [];
+      $scope.loadMore = function() {
+        $ionicLoading.show().then(function(){
+          $http.get('./data/findLoad.json')
+          .success(function(res) {
+            $scope.items = $scope.items.concat( res.reloadlist );
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+            $ionicLoading.hide();
+          });
+        $scope.$on('stateChangeSuccess', function() {
+          console.log('没有更多数据');
+        });
+      });
+    }
+        // 点击事件
+    $scope.tips = function(){
+      $ionicLoading.show(
+        {
+          template:"请先登录"
+        }
+      ).then(function () {
+        $timeout(function () {
+            $ionicLoading.hide();
+          }, 1000);
+      })
+    }
   }])
